25c1ec806b888329b4f32d319afe784d3c729fa5
Marc Lainez <marc.lainez@gmail.com>
2012-10-16 15:38:34 +0200

    Update coffeescript

--- public/javascripts/bookr.js
+++ public/javascripts/bookr.js
@@ -1,139 +1,226 @@
-  $(function() {
-    window.Book = Backbone.Model.extend({
-      toggle: function() {
-        return this.save({
-          selected: !this.get("selected")
-        });
-      },
-      clear: function() {
-        this.destroy();
-        return $(this.view.el).remove();
+(function() {
+  var App, Books, googleApiOnload;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  googleApiOnload = function() {
+    gapi.client.setApiKey("AIzaSyAmiUyoMClSzHIRs47csnhCtC4QAK6FHBQ");
+    return gapi.client.load('books', 'v1');
+  };
+
+  window.googleApiOnload = googleApiOnload;
+
+  window.Bookr = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {}
+  };
+
+  Bookr.Models.Book = (function() {
+
+    __extends(Book, Backbone.Model);
+
+    function Book() {
+      Book.__super__.constructor.apply(this, arguments);
+    }
+
+    Book.prototype.toggle = function() {
+      return this.save({
+        selected: !this.get("selected")
+      });
+    };
+
+    Book.prototype.clear = function() {
+      this.destroy();
+      return $(this.view.el).remove();
+    };
+
+    return Book;
+
+  })();
+
+  Bookr.Collections.BookList = (function() {
+
+    __extends(BookList, Backbone.Collection);
+
+    function BookList() {
+      BookList.__super__.constructor.apply(this, arguments);
+    }
+
+    BookList.prototype.model = Bookr.Models.Book;
+
+    BookList.prototype.url = "/books";
+
+    BookList.prototype.selected = function() {
+      return this.filter(function(book) {
+        return book.get("selected");
+      });
+    };
+
+    BookList.prototype.nextOrder = function() {
+      if (!this.length) return 1;
+      return this.last().get("order") + 1;
+    };
+
+    BookList.prototype.comparator = function(book) {
+      return book.get("order");
+    };
+
+    BookList.prototype.pluralize = function(count) {
+      if (count === 1) {
+        return "book";
+      } else {
+        return "books";
       }
-    });
-    window.BookList = Backbone.Collection.extend({
-      model: Book,
-      url: "/books",
-      selected: function() {
-        return this.filter(function(book) {
-          return book.get("selected");
+    };
+
+    return BookList;
+
+  })();
+
+  Books = new Bookr.Collections.BookList;
+
+  Bookr.Views.AppView = (function() {
+
+    __extends(AppView, Backbone.View);
+
+    function AppView() {
+      AppView.__super__.constructor.apply(this, arguments);
+    }
+
+    AppView.prototype.el = $("#bookr");
+
+    AppView.prototype.statsTemplate = _.template("<% if (total) { %><span class=\"book-count\"><span class=\"number\"><%= remaining %></span><span class=\"word\"> <%= remaining == 1 ? \"book\" : \"books\" %></span> yet to select.</span><% } %><% if (selected) { %><span class=\"book-clear\"><a href=\"#\">Add <span class=\"number-selected\"><%= selected %> </span>selected <span class=\"word-selected\"><%= selected == 1 ? \"book\" : \"books\" %></span></a></span><% } %>");
+
+    AppView.prototype.events = {
+      "keypress #new-book": "createOnEnter",
+      "click .book-clear": "clearCompleted"
+    };
+
+    AppView.prototype.initialize = function() {
+      _.bindAll(this, "addOne", "addAll", "render");
+      this.input = this.$("#new-book");
+      Books.bind("add", this.addOne);
+      Books.bind("reset", this.addAll);
+      Books.bind("all", this.render);
+      Books.fetch();
+      return this;
+    };
+
+    AppView.prototype.render = function() {
+      var selected;
+      selected = Books.selected().length;
+      return this.$("#book-stats").html(this.statsTemplate({
+        selected: selected,
+        total: Books.length,
+        remaining: Books.length - selected
+      }));
+    };
+
+    AppView.prototype.addOne = function(book) {
+      var view;
+      view = new Bookr.Views.BookView({
+        model: book
+      }).render().el;
+      return this.$("#book-list").prepend(view);
+    };
+
+    AppView.prototype.addAll = function() {
+      return Books.each(this.addOne);
+    };
+
+    AppView.prototype.createOnEnter = function(e) {
+      var isbn, request;
+      if (e.keyCode === 13) {
+        isbn = this.input.val();
+        request = gapi.client.books.volumes.list({
+          'q': {
+            'isbn': isbn
+          }
         });
-      },
-      nextOrder: function() {
-        if (!this.length) return 1;
-        return this.last().get("order") + 1;
-      },
-      comparator: function(book) {
-        return book.get("order");
-      },
-      pluralize: function(count) {
-        if (count === 1) {
-          return "book";
-        } else {
-          return "books";
-        }
-      }
-    });
-    window.Books = new BookList;
-    window.BookView = Backbone.View.extend({
-      tagName: "li",
-      className: "book",
-      template: _.template("<input type='checkbox' class='book-check' /><div class='book'></div><span class='book-destroy'></span><input type='text' class='book-input' />"),
-      events: {
-        "click .book-check": "toggleSelected",
-        "click .book-destroy": "clear"
-      },
-      initialize: function() {
-        _.bindAll(this, "render", "close");
-        this.model.bind("change", this.render);
-        return this.model.view = this;
-      },
-      render: function() {
-        $(this.el).html(this.template(this.model.toJSON()));
-        $(this.el).attr("id", "book-" + this.model.id);
-        this.setContent();
-        return this;
-      },
-      setContent: function() {
-        var isbn;
-        title = this.model.get("title");
-        this.$(".book").html(title);
-        this.$(".book-input").val(isbn);
-        if (this.model.get("selected")) {
-          this.$(".book-check").prop("checked", true);
-          $(this.el).addClass("selected");
-        } else {
-          this.$(".book-check").prop("checked", false);
-          $(this.el).removeClass("selected");
-        }
-        return this.input = this.$(".book-input");
-      },
-      toggleSelected: function() {
-        return this.model.toggle();
-      },
-      close: function() {
-        return this.model.save({
-          isbn: this.input.val("value")
-        });
-      },
-      clear: function() {
-        return this.model.clear();
-      }
-    });
-    window.AppView = Backbone.View.extend({
-      el: $("#bookr"),
-      statsTemplate: _.template("<% if (total) { %><span class=\"book-count\"><span class=\"number\"><%= remaining %></span><span class=\"word\"> <%= remaining == 1 ? \"book\" : \"books\" %></span> yet to select.</span><% } %><% if (selected) { %><span class=\"book-clear\"><a href=\"#\">Add <span class=\"number-selected\"><%= selected %> </span>selected <span class=\"word-selected\"><%= selected == 1 ? \"book\" : \"books\" %></span></a></span><% } %>"),
-      events: {
-        "keypress #new-book": "createOnEnter",
-        "click .book-clear": "clearCompleted"
-      },
-      initialize: function() {
-        _.bindAll(this, "addOne", "addAll", "render");
-        this.input = this.$("#new-book");
-        Books.bind("add", this.addOne);
-        Books.bind("reset", this.addAll);
-        Books.bind("all", this.render);
-        Books.fetch();
-        return this;
-      },
-      render: function() {
-        var selected;
-        selected = Books.selected().length;
-        return this.$("#book-stats").html(this.statsTemplate({
-          selected: selected,
-          total: Books.length,
-          remaining: Books.length - selected
-        }));
-      },
-      addOne: function(book) {
-        var view;
-        view = new BookView({
-          model: book
-        }).render().el;
-        return this.$("#book-list").prepend(view);
-      },
-      addAll: function() {
-        return Books.each(this.addOne);
-      },
-      createOnEnter: function(e) {
-        if (e.keyCode !== 13) return;
-        var isbn = this.input.val();
-        var request = gapi.client.books.volumes.list({'q': {'isbn': isbn}});
-        request.execute(function(response){
-          Books.create(response.result.items[0].volumeInfo);
+        request.execute(function(response) {
+          return Books.create(response.result.items[0].volumeInfo);
         });
         return this.input.val("");
-      },
-      clearCompleted: function() {
-        _.each(Books.selected(), function(book) {
-          return book.clear();
-        });
-        return false;
       }
-    });
-    return window.App = new AppView;
-  });
+    };
 
-  function GoogleApiOnload(){
-    gapi.client.setApiKey("AIzaSyAmiUyoMClSzHIRs47csnhCtC4QAK6FHBQ");
-    gapi.client.load('books', 'v1');
-  };
+    AppView.prototype.clearCompleted = function() {
+      _.each(Books.selected(), function(book) {
+        return book.clear();
+      });
+      return false;
+    };
+
+    return AppView;
+
+  })();
+
+  App = new Bookr.Views.AppView;
+
+  Bookr.Views.BookView = (function() {
+
+    __extends(BookView, Backbone.View);
+
+    function BookView() {
+      BookView.__super__.constructor.apply(this, arguments);
+    }
+
+    BookView.prototype.tagName = "li";
+
+    BookView.prototype.className = "book";
+
+    BookView.prototype.template = _.template("<input type='checkbox' class='book-check' /><div class='book'></div><span class='book-destroy'></span><input type='text' class='book-input' />");
+
+    BookView.prototype.events = {
+      "click .book-check": "toggleSelected",
+      "click .book-destroy": "clear"
+    };
+
+    BookView.prototype.initialize = function() {
+      _.bindAll(this, "render", "close");
+      this.model.bind("change", this.render);
+      return this.model.view = this;
+    };
+
+    BookView.prototype.render = function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      $(this.el).attr("id", "book-" + this.model.id);
+      this.setContent();
+      return this;
+    };
+
+    BookView.prototype.setContent = function() {
+      var title;
+      title = this.model.get("title");
+      this.$(".book").html(title);
+      this.$(".book-input").val("");
+      if (this.model.get("selected")) {
+        this.$(".book-check").prop("checked", true);
+        $(this.el).addClass("selected");
+      } else {
+        this.$(".book-check").prop("checked", false);
+        $(this.el).removeClass("selected");
+      }
+      return this.$(".book-input");
+    };
+
+    BookView.prototype.toggleSelected = function() {
+      return this.model.toggle();
+    };
+
+    BookView.prototype.close = function() {
+      return this.model.save({
+        isbn: this.input.val("value")
+      });
+    };
+
+    BookView.prototype.clear = function() {
+      return this.model.clear();
+    };
+
+    return BookView;
+
+  })();
+
+}).call(this);
